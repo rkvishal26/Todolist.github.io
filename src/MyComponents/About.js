00074998a4a1c81bd01c3674c390bd4f4577273f
Vishal Singh <rkvishalsingh26@gmail.com>
2021-07-30 09:20:54 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            
+            <center><p><strong>This is under construction Page, Do check soon</strong></p></center>
+        </div>
+    )
+}
